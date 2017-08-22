d77944483e0c259c5065c54b798cf395303babf6
Matthew Duek <mashuduek@gmail.com>
2017-08-22 10:04:28 -0400

    Add user controller and static page controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["nvalid username/pw combo"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      # render "api/users/show"
+      render json: {}
+    else
+      render json: ["nobody here"], status: 404
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822130314) do
+ActiveRecord::Schema.define(version: 20170822131234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
