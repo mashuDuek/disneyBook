d4d197d3f4e275731adbb548fc677fb31b9bbce7
Matthew Duek <mashuduek@gmail.com>
2018-05-08 11:10:48 -0400

    fix AWS s3 in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,11 +54,11 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :bucket => ENV["s3_bucket"],
-    :s3_region => "us-east-1",
+    :bucket => ENV['s3_bucket'],
+    :s3_region => ENV['AWS_REGION'],
     :s3_credentials => {
-      :access_key_id => ENV["s3_access_key_id"],
-      :secret_access_key => ENV["s3_secret_access_key"]
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
